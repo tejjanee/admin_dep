f8aeaf5bf60b5a47cc0e0ccb16fb4187cfaf65df
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-08-19 14:39:23 +0530

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "https://dep-back-s4j5.onrender.com/api/v1/message/getall",
+          "https://back-dep.onrender.com/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
