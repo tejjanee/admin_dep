d5de2bb098075b7f9e105c135110d5049b05f0f0
TEJA RAM <135523042+tejjanee@users.noreply.github.com>
2024-08-19 14:38:47 +0530

    Update Doctors.jsx

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
     const fetchDoctors = async () => {
       try {
         const { data } = await axios.get(
-          "https://dep-back-s4j5.onrender.com/api/v1/user/doctors",
+          "https://back-dep.onrender.com/api/v1/user/doctors",
           { withCredentials: true }
         );
         setDoctors(data.doctors);
